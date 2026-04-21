96b935564ee280a2767fe9973d630ab9c889ba19
MikeWang1224 <166575039+MikeWang1224@users.noreply.github.com>
2026-04-21 20:53:11 +0800

    Initial Commit

---
+++ TarotApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TarotApp
+//
+//  Created by wang  on 2026/4/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TarotApp/TarotAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TarotAppApp.swift
+//  TarotApp
+//
+//  Created by wang  on 2026/4/21.
+//
+
+import SwiftUI
+
+@main
+struct TarotAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
